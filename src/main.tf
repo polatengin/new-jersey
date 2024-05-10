274092eff4ab80c414546deee91b00d2ffdece18
polatengin <enpolat@microsoft.com>
2024-05-09 18:14:03 -0700

    adding azurerm provider related information

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "deereazdoterraform"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "random_string" "random_suffix" {
   length  = 5
   special = false
