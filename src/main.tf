56b4853f3c91c559e41c632175e271c706ddbf20
polatengin <enpolat@microsoft.com>
2024-05-09 18:53:01 -0700

    formatting document

--- src/main.tf
+++ src/main.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "deereazdoterraform"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "deereazdoterraform"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
